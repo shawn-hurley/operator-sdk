97a90b070fec52eaa74de7c3895adf4dbaf0d668
Shawn Hurley <smhurley00@gmail.com>
2018-09-13 11:51:08 -0400

    testing var

--- pkg/ansible/controller/flags.go
+++ pkg/ansible/controller/flags.go
@@ -12,7 +12,7 @@ func AddPflags(fs *pflag.FlagSet) *Options {
 
 func AddGoFlags(fs *flag.FlagSet) *Options {
 	o := &Options{}
-	i := o.LoggingLevel
-	fs.Int("runner-loglevel", int(i), "The level for the ansible operator event logging, 0 for tasks, 1 for everyting and >1 for nothing")
+	i := int(o.LoggingLevel)
+	fs.IntVar(&i, "runner-loglevel", 0, "The level for the ansible operator event logging, 0 for tasks, 1 for everyting and >1 for nothing")
 	return o
 }
